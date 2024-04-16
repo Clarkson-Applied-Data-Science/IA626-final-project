e0c88f5ce9fdbc402c0ab9849db3f761b4cb1281
Sebastian Geiger <geigersr@clarkson.edu>
2024-04-16 09:07:33 -0400

    Add another endpoint

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import json,pymysql,time
 from flask import Flask
 from flask import request,redirect
+import matplotlib.pyplot as plt
+import numpy as np
 
 app = Flask(__name__)
 res = {}
@@ -40,7 +42,7 @@ def getPlayer():
         res['rows'] = rows
     else:
         res['code'] = 0
-        res['msg'] = 'Please Enter a Valid NHL Player Name'
+        res['msg'] = 'Please enter a valid NHL player name'
         res['rows'] = None
     return json.dumps(res,indent=4)
 
@@ -67,7 +69,66 @@ def getTopScorers():
         res['rows'] = rows
     else:
         res['code'] = 0
-        res['msg'] = 'Please Enter a Year between 2007 and 2022'
+        res['msg'] = 'Please enter a year between 2007 and 2022'
+        res['rows'] = None
+    return json.dumps(res,indent=4)
+
+@app.route("/getTeamShootingPct", methods=['GET','POST'])
+def getTeamShootingPct():
+    conn = pymysql.connect(host='mysql.clarksonmsda.org',port=3306,user='ia626',passwd='ia626clarkson', db='ia626', autocommit=True)
+    cur = conn.cursor(pymysql.cursors.DictCursor)
+    sql = '''SELECT `teamCode`, ROUND(SUM(`goal`) / COUNT(`goal`) * 100,0) AS `shootingPct` FROM `geigersr_shots` WHERE `season` = %s
+                GROUP BY `teamCode` ORDER BY `ShootingPct` DESC;'''
+    season = request.args.get('season')
+    print(season)
+    token = f"{season}"
+    cur.execute(sql,(token))
+    rows = []
+    for row in cur:
+        d = {}
+        d['team'] = row['teamCode']
+        d['shootingPct'] = int(row['shootingPct']) 
+        rows.append(d)
+    if len(rows) > 0:
+        res['code'] = 1
+        res['msg'] = 'ok'
+        res['req'] = '/getTeamShootingPct'
+        res['rows'] = rows
+    else:
+        res['code'] = 0
+        res['msg'] = 'Please enter a year between 2007 and 2022'
+        res['rows'] = None
+    return json.dumps(res,indent=4)
+
+@app.route("/getPlayerChart", methods=['GET','POST'])
+def getPlayerChart():
+    conn = pymysql.connect(host='mysql.clarksonmsda.org',port=3306,user='ia626',passwd='ia626clarkson', db='ia626', autocommit=True)
+    cur = conn.cursor(pymysql.cursors.DictCursor)
+    sql = '''SELECT s.arenaAdjustedXCord, s.arenaAdjustedYCord, s.event, p.name FROM geigersr_shots s, geigersr_players p
+            WHERE s.shooterId = p.playerId AND p.name = %s AND s.season = %s;'''
+    name = request.args.get('name')
+    season = request.args.get('season')
+    print(name, season)
+    tokens = [name, season]
+    cur.execute(sql,(tokens))
+    rows = []
+    for row in cur:
+        d = {}
+        d['xcord'] = row['arenaAdjustedXCord']
+        d['ycord'] = row['arenaAdjustedYCord']
+        d['event'] = row['event']
+        d['name'] = row['name']
+        rows.append(d)
+    
+    if len(rows) > 0:
+        res['code'] = 1
+        res['msg'] = 'ok'
+        res['req'] = '/getPlayerChart'
+        res['rows'] = rows
+
+    else:
+        res['code'] = 0
+        res['msg'] = 'Please enter a year between 2007 and 2022 and a valid NHL player name'
         res['rows'] = None
     return json.dumps(res,indent=4)
 
--- sqltests.sql
+++ sqltests.sql
@@ -8,3 +8,16 @@ ORDER BY `Goals` DESC
 LIMIT 0,10;
 
 
+SELECT `teamCode`, ROUND(SUM(`goal`) / COUNT(`goal`) * 100,0) AS `shootingPct` 
+FROM `geigersr_shots` 
+WHERE `season` = %s
+GROUP BY `teamCode` 
+ORDER BY `ShootingPct` DESC;
+
+
+
+SELECT s.arenaAdjustedXCord, s.arenaAdjustedYCord, s.event, p.name
+FROM geigersr_shots s, geigersr_players p
+WHERE s.shooterId = p.playerId
+AND p.name = 'Connor McDavid'
+AND s.season = 2022
